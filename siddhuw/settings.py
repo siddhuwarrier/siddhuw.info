33949a5519b5e856ad50da30fdfad4dd67930f81
siddhu.warrier <siddhu@siddhuw.info>
2012-08-22 22:16:59 +0100

    - Add packages required for Heroku - set database to postgres - Remove virtual env from git; sorry, committed by mistake

--- siddhuw/settings.py
+++ siddhuw/settings.py
@@ -1,4 +1,7 @@
 # Django settings for siddhuw project.
+import dj_database_url
+
+
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -10,15 +13,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+	'default': dj_database_url.config(default='postgres://localhost')}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
