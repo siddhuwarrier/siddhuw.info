dce0f0de5af7250742fe5c8facf177982be82e39
siddhu.warrier <siddhu@siddhuw.info>
2012-08-26 13:27:50 +0100

    0.1.2
    - Enable serving static files
    - Enable admin module

--- urls.py
+++ urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic.base import TemplateView
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name="index.html")),
 )
+
+urlpatterns += staticfiles_urlpatterns()
