e9420d8f2dd61877edc33d7b7dad95c34aa918fd
Siddhu Warrier <siwarrie@cisco.com>
2026-02-19 21:46:27 +0000

    feat(infra): add basic Terraform config (#3)
    * feat(infra): add basic Terraform config
    
    * Mark API token as sensitive

---
+++ infra/variables.tf
@@ -0,0 +1,9 @@
+variable "cloudflare_api_token" {
+  description = "My Cloudflare API token"
+  type = string
+  sensitive = true
+}
+
+variable "cloudflare_account_id" {
+  description = "My Cloudflare account ID"
+}
