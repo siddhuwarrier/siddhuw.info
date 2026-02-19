64682ffd0fb47853c300a8461825897bcf7c6627
Siddhu Warrier <siwarrie@cisco.com>
2026-02-19 21:37:23 +0000

    feat(infra): add basic Terraform config

---
+++ infra/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+  # pulls API token from env var CLOUDFLARE_API_TOKEN
+}
---
+++ infra/variables.tf
@@ -0,0 +1,8 @@
+variable "cloudflare_api_token" {
+  description = "My Cloudflare API token"
+  type = string
+}
+
+variable "cloudflare_account_id" {
+  description = "My Cloudflare account ID"
+}
