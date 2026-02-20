1e357e61dc1e412d4a4a2e936286d0c654497cba
Siddhu Warrier <siwarrie@cisco.com>
2026-02-20 20:46:24 +0000

    feat(r2): make R2 bucket with CV publicly accessible

--- infra/storage.tf
+++ infra/storage.tf
@@ -3,3 +3,11 @@ resource "cloudflare_r2_bucket" "cloudflare-bucket" {
   name       = var.storage_bucket_name
   location   = var.storage_bucket_location
 }
+
+resource "cloudflare_r2_custom_domain" "assets" {
+  account_id = var.cloudflare_account_id
+  zone_id = var.cloudflare_zone_id
+  bucket_name  = cloudflare_r2_bucket.cloudflare-bucket.name
+  domain     = "assets.${var.domain}"
+  enabled = true
+}
