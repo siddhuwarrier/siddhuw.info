20c5167c9419b64bda8c8fa6a10830e373b7c3be
Siddhu Warrier <siwarrie@cisco.com>
2026-03-06 06:50:05 +0000

    Test out API paths

--- workers/email-validation-worker/src/index.ts
+++ workers/email-validation-worker/src/index.ts
@@ -7,39 +7,52 @@ import { validateTurnstile } from "./services/turnstile-validation.service";
 
 export default {
 	async fetch(request: Request, env: Env): Promise<Response> {
-		if (request.method !== "POST") {
-			return new Response("Method not allowed", { status: 405 });
+		const url = new URL(request.url);
+
+		switch (url.pathname) {
+			case '/validate':
+				if (request.method !== "POST") {
+					return new Response("Method not allowed", { status: 405 });
+				}
+				return Response.json({status: "success"})
+			case '/hello':
+				return Response.json({status: "burak-crush-pineapple"})
+			default:
+				return new Response("Not found", { status: 404 })
 		}
+		// if (request.method !== "POST") {
+		// 	return new Response("Method not allowed", { status: 405 });
+		// }
 
-		const body = await request.json<ContactFormBody>();
-		const { turnstileToken, fromEmail, contactMessage } = body;
+		// const body = await request.json<ContactFormBody>();
+		// const { turnstileToken, fromEmail, contactMessage } = body;
 
-		// Validate Turnstile token
-		if (!(await validateTurnstile(env.TURNSTILE_SECRET_KEY, turnstileToken))) {
-			return new Response("Turnstile validation failed", { status: 403 });
-		}
+		// // Validate Turnstile token
+		// if (!(await validateTurnstile(env.TURNSTILE_SECRET_KEY, turnstileToken))) {
+		// 	return new Response("Turnstile validation failed", { status: 403 });
+		// }
 
-		// Create MIME message
-		const msg = createMimeMessage();
-		msg.setSender({ name: "Cloudflare E-mailer", addr: "hi@mail.siddhuw.uk" });
-		msg.setRecipient("siddhu@siddhuw.info");
-		msg.setSubject("New contact form submission");
-		msg.addMessage({
-			contentType: "text/plain",
-			data: `From: ${fromEmail}\n\n${contactMessage}`,
-		});
+		// // Create MIME message
+		// const msg = createMimeMessage();
+		// msg.setSender({ name: "Cloudflare E-mailer", addr: "hi@mail.siddhuw.uk" });
+		// msg.setRecipient("siddhu@siddhuw.info");
+		// msg.setSubject("New contact form submission");
+		// msg.addMessage({
+		// 	contentType: "text/plain",
+		// 	data: `From: ${fromEmail}\n\n${contactMessage}`,
+		// });
 
-		// Send email
-		const message = new EmailMessage(
-			"hi@mail.siddhuw.uk",
-			"siddhu@siddhuw.info",
-			msg.asRaw(),
-		);
-		await env.cloudflare_emailer.send(message);
+		// // Send email
+		// const message = new EmailMessage(
+		// 	"hi@mail.siddhuw.uk",
+		// 	"siddhu@siddhuw.info",
+		// 	msg.asRaw(),
+		// );
+		// await env.cloudflare_emailer.send(message);
 
-		return Response.json({
-			status: "success",
-			message: "Email sent",
-		});
+		// return Response.json({
+		// 	status: "success",
+		// 	message: "Email sent",
+		// });
 	},
 };
